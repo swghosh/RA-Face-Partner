181449247dc2c9e3a62d8eccb759b6e3aec384d5
SwG Ghosh <codecrafts.cf@icloud.com>
2019-07-22 22:03:21 +0530

    Added CVWrapper.mm with bridging header for Swift support

---
+++ RA Face Partner/CVWrapper.h
@@ -0,0 +1,17 @@
+//
+//  CVWrapper.h
+//  RA Face Partner
+//
+//  Created by Swarup Ghosh on 22/07/19.
+//  Copyright © 2019 Swarup Ghosh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CVWrapper : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RA Face Partner/CVWrapper.mm
@@ -0,0 +1,14 @@
+//
+//  CVWrapper.mm
+//  RA Face Partner
+//
+//  Created by Swarup Ghosh on 22/07/19.
+//  Copyright © 2019 Swarup Ghosh. All rights reserved.
+//
+
+#import <opencv2/opencv.hpp>
+#import "CVWrapper.h"
+
+@implementation CVWrapper
+
+@end
