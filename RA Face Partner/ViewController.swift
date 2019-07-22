81b77d7bd8a20739d793f15dc2f75a7aa30272e1
SwG Ghosh <codecrafts.cf@icloud.com>
2019-07-22 21:59:12 +0530

    Initial commit

---
+++ RA Face Partner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RA Face Partner
+//
+//  Created by Swarup Ghosh on 22/07/19.
+//  Copyright © 2019 Swarup Ghosh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
